142eb8d99c082d40bd794af7170d7130b88a80c2
brownguo <yexuan.guo@codedance.cn>
2019-05-24 22:48:53 +0800

    404 page

--- passport/main_t.go
+++ passport/main_t.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kataras/iris"
 	"go-commons/passport/controller"
 )
 
 func main(){
 
-	fmt.Printf("Hello Test Page!")
+	//fmt.Printf("Hello Test Page!")
 
 	app := iris.New()
 
 	app.RegisterView(iris.HTML("./views",".html"))
+	app.OnErrorCode(iris.StatusNotFound, notFound)
 
 	app.Get("/",func(ctx iris.Context){
 		ctx.ViewData("messages","MainTestPage!,Based var!")
@@ -29,4 +29,9 @@ func main(){
 
 	app.Run(iris.Addr(":8080"))
 
+}
+
+func notFound(ctx iris.Context) {
+	// 出现 404 的时候，就跳转到 $views_dir/errors/404.html 模板
+	ctx.View("notfound.html")
 }
